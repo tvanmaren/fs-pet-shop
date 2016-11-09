7d0219ef673739679a459de05152cc658040e9a8
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-09 00:36:30 -0700

    add full read index functionality

--- pets.js
+++ pets.js
@@ -7,21 +7,27 @@ var node = path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd = process.argv[2];
 
+const baseUse=`Usage ${node} ${file} [read | create | update | destroy]`;
+
 if (cmd === 'read') {
     fs.readFile('./pets.json', 'utf8', (err, data) => {
         if (err) {
             throw err;
         }
         let readOut = JSON.parse(data);
-        let index=process.argv[3];
-        if (index) {
-          console.log(readOut[+index]);
+        let index = +process.argv[3];
+        if (isNaN(index)) {
+          console.log(readOut);
+        }
+        else if (index>=0 && index<readOut.length) {
+            console.log(readOut[index]);
         } else {
-        console.log(readOut);
-    }});
+            console.error(baseUse+' INDEX');
+        }
+    });
 } else {
     if (!cmd) {
-        console.error(`Usage ${node} ${file} [read | create | update | destroy]`);
+        console.error(baseUse);
         process.exit(1);
     }
 }
