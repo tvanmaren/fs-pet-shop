5e8ea37af6c1e5522fec5f696af39fdda0c30a91
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-09 00:00:18 -0700

    add read functionality

--- pets.js
+++ pets.js
@@ -1,13 +1,20 @@
 'use strict';
 
-// var fs=require('fs');
+var fs=require('fs');
 var path=require('path');
 
 var node=path.basename(process.argv[0]);
 var file = path.basename(process.argv[1]);
 var cmd=process.argv[2];
 
-if (!cmd) {
-  console.error(`Usage ${node} ${file} [read | create | update | destroy]`);
-  process.exit(1);
+if (cmd==='read') {
+  fs.readFile('./pets.json', 'utf8', (err, data) => {if (err) {throw err;}
+  let readOut=JSON.parse(data);
+  console.log(readOut);
+});
+} else {
+  if (!cmd) {
+    console.error(`Usage ${node} ${file} [read | create | update | destroy]`);
+    process.exit(1);
+  }
 }
