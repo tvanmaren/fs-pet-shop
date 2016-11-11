82e9195f3f372cef6e4adec2de880283e79c406f
Tristan Van Maren <tm.vanmaren@gmail.com>
2016-11-11 10:19:46 -0700

    complete post functionality & erroring

--- expressServer.js
+++ expressServer.js
@@ -2,9 +2,9 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
 const port = 8000;
-
-// const bodyhandler=require('body-handler');
 
 const path = require('path');
 const fs = require('fs');
@@ -34,7 +34,7 @@ app.get('/pets/:id', (req, res, next) => {
         let dataStore = JSON.parse(data);
         console.log(dataStore.length);
         if (isNaN(index) || index < 0 || index >= dataStore.length) {
-            res.set('Content-Type', 'text/plain');
+            // res.set('Content-Type', 'text/plain');
             res.sendStatus(404);
         }
 
@@ -42,8 +42,28 @@ app.get('/pets/:id', (req, res, next) => {
     });
 });
 
-app.post('/pets/:id', (req, res, next) => {
+app.post('/pets', (req, res, next) => {
+    let index = +req.params.id;
+    const newPet = req.body;
 
+    if (!newPet || !(newPet.age && typeof newPet.age==='number') || !(newPet.kind && typeof newPet.kind==='string') || !(newPet.name && typeof newPet.name==='string')) {
+        return res.sendStatus(400);
+    }
+
+    fs.readFile(database, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err.stack);
+            return res.sendStatus(500);
+        }
+        let dataStore = JSON.parse(data);
+        dataStore.push(newPet);
+        fs.writeFile(database, JSON.stringify(dataStore), (err) => {
+            if (err) {
+                return res.sendStatus(500);
+            }
+            return res.status(200).send(newPet);
+        });
+    });
 });
 
 app.put('/pets:id', (req, res, next) => {
